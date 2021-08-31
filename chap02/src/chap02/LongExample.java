6445c0ceb08059ffd7ead18c298adc7b85521e00
정현주 <48791736+hyeonju9705@users.noreply.github.com>
2021-08-31 17:56:00 +0900

    ~75p / ~비트반전 연산자(~)

---
+++ chap02/src/chap02/LongExample.java
@@ -0,0 +1,16 @@
+package chap02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		// long타입 변수
+		long var1 = 10;
+		long var2 = 20L;
+		long var4 = 1000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
